1d0f8c7f799aa918978d0bff9366fac3fa1f97de
constanzabassi <cdb66@pitt.edu>
2024-04-26 13:08:38 -0400

    bug fix

--- get_virmen_iterations_and_times_digidata_positive_peaks.m
+++ get_virmen_iterations_and_times_digidata_positive_peaks.m
@@ -1,9 +1,5 @@
-function [acquisitions,trial_its,sound_condition_array]  = get_virmen_iterations_and_times_digidata_positive_peaks(base, virmen_channel_number,string,sound_condition_array,data,file_trial_ids)
-[trial_its,~] = virmen_it_rough_estimation(data); 
-%base = "\\136.142.49.216\Runyan2\Connie\2p_results\DM1-00\wavesurfer\2021-05-20"
-
+function [acquisitions,updated_trial_its,sound_condition_array]  = get_virmen_iterations_and_times_digidata_positive_peaks(base, virmen_channel_number,string,sound_condition_array,data,file_trial_ids,file_estimated_trial_info)
 file_ind = 0;
-
 
 %initialize variables
 possible_it_times = [];
@@ -12,6 +8,9 @@ possible_iterations = [];
 cd(base);
 z = dir(strcat('*',string,'*.abf'));
 num_files = length(z);
+
+[updated_trial_its] = virmen_it_rough_estimation(data);
+
 for n = 1:num_files  %%%%still need to deal with iteration #1
   %keep n file_ind z virmen_channel_number acquisitions string
   %if contains(z(n).name,string)==1
@@ -36,7 +35,7 @@ for n = 1:num_files  %%%%still need to deal with iteration #1
     if isempty(marked_its)
         disp('Error! Current file does not have positive values. File is probably too short')
         n=n+1;
-        file_ind = file_ind +1;
+%         file_ind = file_ind +1;
     else
     
     %findpeaks could find peaks right next to each other this code will
@@ -94,7 +93,7 @@ for n = 1:num_files  %%%%still need to deal with iteration #1
     
     %if first 10000?
     if round(it_values(marked_its(1)),0)==10 %round(it_values(marked_its(1)),1)==10 was set to 1 instead of 0
-        actual_it_values(marked_its(1)) = 1;
+        actual_it_values(marked_its(1)) = 1; %might need to make zero to deal with negative iteration which is not part of the true iterations
          
     else
         actual_it_values(marked_its(1)) = round(((it_values(marked_its(1))*1e5)/1e4))*1e4;
@@ -118,45 +117,28 @@ for n = 1:num_files  %%%%still need to deal with iteration #1
                     previous_value = actual_it_values(it);
                 end
            end
-%            if round(it_values(marked_its(1)),1)==0.1
-%                 if round(previous_value+1,-4)==round(((it_values(marked_its(m-1))*1e5)/1e4))*1e4
-%                    if m <length(marked_its) 
-%                        for it = marked_its(m):marked_its(m+1)%length(it_values)%marked_its(m+1)-1%marked_its(m):marked_its(m+1) %have to figure out numbers
-%                             actual_it_values(it) = previous_value+1;
-%                             previous_value = actual_it_values(it);
-%                        end
-%                    else
-%                         for it = marked_its(m):length(it_values)%%have to figure out numbers
-%                             actual_it_values(it) = previous_value+1;
-%                             previous_value = actual_it_values(it);
-%                         end
-%                    end
-%                 else
-%                     disp('Error! Iteration numbers do not match')
-%                     keyboard
-%                 end
-%            else
+
            if round(previous_value+1,-4)==round(((it_values(marked_its(m))*1e5)/1e4))*1e4
                if m <length(marked_its) 
-                   for it = marked_its(m):marked_its(m+1)%length(it_values)%marked_its(m+1)-1%marked_its(m):marked_its(m+1) %have to figure out numbers
+                   for it = marked_its(m):marked_its(m+1)-1%length(it_values)%marked_its(m+1)-1%marked_its(m):marked_its(m+1) %have to figure out numbers
                         actual_it_values(it) = previous_value+1;
                         previous_value = actual_it_values(it);
                    end
                else
                     for it = marked_its(m):length(it_values)%%have to figure out numbers
-                        actual_it_values(it) = round(((it_values(marked_its(m))*1e5)/1e4))*1e4+1;
+                        actual_it_values(it) = previous_value+1;
                         previous_value = actual_it_values(it);
                     end
                end
             else
                 if m <length(marked_its) 
-                   for it = marked_its(m):marked_its(m+1)%length(it_values)%marked_its(m+1)-1%marked_its(m):marked_its(m+1) %have to figure out numbers
-                        actual_it_values(it) = round(((it_values(marked_its(m))*1e5)/1e4))*1e4+1;
+                   for it = marked_its(m):marked_its(m+1)-1%length(it_values)%marked_its(m+1)-1%marked_its(m):marked_its(m+1) %have to figure out numbers
+                        actual_it_values(it) = previous_value+1;%round(((it_values(marked_its(m))*1e5)/1e4))*1e4+1;
                         previous_value = actual_it_values(it);
                    end
                else
                     for it = marked_its(m):length(it_values)%%have to figure out numbers
-                        actual_it_values(it) = round(((it_values(marked_its(m))*1e5)/1e4))*1e4+1;
+                        actual_it_values(it) = previous_value+1;%round(((it_values(marked_its(m))*1e5)/1e4))*1e4+1;
                         previous_value = actual_it_values(it);
                     end
                end
@@ -169,59 +151,102 @@ for n = 1:num_files  %%%%still need to deal with iteration #1
         end
         actual_it_values(length(actual_it_values))
     end
-    
+
+    %CHECK TO MAKE YOU ARE NOT SKIPPING ANY ITERATIONS!!
+    if ~isempty(find(diff(actual_it_values)>1)) || ~isempty(find(diff(actual_it_values)==0))
+        error('Error. n\Some iterations are missing');
+    end
     %
     start_trial_number = file_trial_ids(file_ind,1);%+file_trial_ids(file,3)-2; 
     end_trial_number = file_trial_ids(file_ind,2);
 
+    %adjust iterations from virmen using actual recorded iterations! to
+    %figure out true start iti and end trial iterations and keep track of
+    %them for each file
+    [trial_its,~] = virmen_it_rough_estimation_adjusted(data,sync_sampling_rate,it_times,actual_it_values); 
+    if start_trial_number == 1
+        updated_trial_its.end_trial_its(start_trial_number:end_trial_number+1) = trial_its.end_trial_its(start_trial_number:end_trial_number+1);
+        updated_trial_its.end_iti_its(start_trial_number:end_trial_number+1) = trial_its.end_iti_its(start_trial_number:end_trial_number+1);
+        updated_trial_its.start_trial_its(start_trial_number:end_trial_number+1) = trial_its.start_trial_its(start_trial_number:end_trial_number+1);
+        updated_trial_its.start_iti_its(start_trial_number:end_trial_number+1) = trial_its.start_iti_its(start_trial_number:end_trial_number+1);
+    elseif end_trial_number == length(trial_its)
+        updated_trial_its.end_trial_its(start_trial_number-1:end_trial_number) = trial_its.end_trial_its(start_trial_number-1:end_trial_number);
+        updated_trial_its.end_iti_its(start_trial_number-1:end_trial_number) = trial_its.end_iti_its(start_trial_number-1:end_trial_number);
+        updated_trial_its.start_trial_its(start_trial_number-1:end_trial_number) = trial_its.start_trial_its(start_trial_number-1:end_trial_number);
+        updated_trial_its.start_iti_its(start_trial_number-1:end_trial_number) = trial_its.start_iti_its(start_trial_number-1:end_trial_number);
+    else
+        updated_trial_its.end_trial_its(start_trial_number-1:end_trial_number+1) = trial_its.end_trial_its(start_trial_number-1:end_trial_number+1);
+        updated_trial_its.end_iti_its(start_trial_number-1:end_trial_number+1) = trial_its.end_iti_its(start_trial_number-1:end_trial_number+1);
+        updated_trial_its.start_trial_its(start_trial_number-1:end_trial_number+1) = trial_its.start_trial_its(start_trial_number-1:end_trial_number+1);
+        updated_trial_its.start_iti_its(start_trial_number-1:end_trial_number+1) = trial_its.start_iti_its(start_trial_number-1:end_trial_number+1);
+    end
+    
+    
     %assign iterations within file limits!
-    possible_iterations = trial_its.start_trial_its(start_trial_number):trial_its.end_iti_its(end_trial_number); %limit iterations to ones within imaging frames// this is iterations ids
+    possible_iterations = updated_trial_its.start_trial_its(start_trial_number):updated_trial_its.end_iti_its(end_trial_number); %limit iterations to ones within imaging frames// this is iterations ids
     possible_it_locs = find(ismember(actual_it_values,possible_iterations)); %locations of iterations within limits
 
+    %include iti before first trial and full maze trial at the end if possible?
+%     if file_trial_ids(file_ind,5) == 1 && file_trial_ids(file_ind,6) == 1
+%         possible_iterations = trial_its.start_iti_its(start_trial_number-1):trial_its.end_trial_its(end_trial_number+1); %limit iterations to ones within imaging frames// this is iterations ids
+%         possible_it_locs = find(ismember(actual_it_values,possible_iterations)); %locations of iterations within limits
+%     elseif file_trial_ids(file_ind,5) == 1
+%         possible_iterations = trial_its.start_iti_its(start_trial_number-1):trial_its.end_iti_its(end_trial_number); %limit iterations to ones within imaging frames// this is iterations ids
+%         possible_it_locs = find(ismember(actual_it_values,possible_iterations)); %locations of iterations within limits
+%     elseif file_trial_ids(file_ind,6) == 1
+%         possible_iterations = trial_its.start_trial_its(start_trial_number):trial_its.end_trial_its(end_trial_number+1); %limit iterations to ones within imaging frames// this is iterations ids
+%         possible_it_locs = find(ismember(actual_it_values,possible_iterations)); %locations of iterations within limits
+%     else
+%         possible_iterations = trial_its.start_trial_its(start_trial_number):trial_its.end_trial_its(end_trial_number); %limit iterations to ones within imaging frames// this is iterations ids
+%         possible_it_locs = find(ismember(actual_it_values,possible_iterations)); %locations of iterations within limits
+%     end
 
     %for dealing with weird zeros before acqusition counter for first file
     if file_ind ==1
         if length(find(actual_it_values==0)) >0
-        no_zeros = find(actual_it_values>0);
-        no_zeros = [no_zeros(1)-1,no_zeros];
-        acquisitions(file_ind).actual_it_values = actual_it_values(possible_it_locs);%(no_zeros);
-%         acquisitions(file_ind).it_times = it_times(no_zeros); %unsure if I need this
-        acquisitions(file_ind).it_times = it_times(possible_it_locs);
-        acquisitions(file_ind).directory = z(file_ind).name;
-        %save sound onsets
-        sound_onsets_speakers = [sound_condition_array(file_ind).VR_sounds{:,2}]; 
-        nan_ind = find(isnan(sound_onsets_speakers));
-        sound_onsets_speakers = sound_onsets_speakers(~isnan(sound_onsets_speakers));
-        possible_sound_onsets = sound_onsets_speakers;
-        acquisitions(file_ind).sound_trigger_time = possible_sound_onsets;
+            no_zeros = find(actual_it_values>0);
+            no_zeros = [no_zeros(1)-1,no_zeros];
+            acquisitions(file_ind).actual_it_values = actual_it_values(possible_it_locs);%(no_zeros);
+    %         acquisitions(file_ind).it_times = it_times(no_zeros); %unsure if I need this
+            acquisitions(file_ind).it_times = it_times(possible_it_locs);
+            acquisitions(file_ind).directory = z(file_ind).name;
+            %save sound onsets
+            sound_onsets_speakers = [sound_condition_array(file_ind).VR_sounds{:,2}]; 
+            nan_ind = find(isnan(sound_onsets_speakers));
+            sound_onsets_speakers = sound_onsets_speakers(~isnan(sound_onsets_speakers));
+            possible_sound_onsets = sound_onsets_speakers;
+            acquisitions(file_ind).sound_trigger_time = possible_sound_onsets;
         else
-        acquisitions(file_ind).actual_it_values = actual_it_values(possible_it_locs);
-        acquisitions(file_ind).it_times = it_times(possible_it_locs);
-        acquisitions(file_ind).directory = z(file_ind).name;
-        %save sound onsets
-        sound_onsets_speakers = [sound_condition_array(file_ind).VR_sounds{:,2}]; 
-        nan_ind = find(isnan(sound_onsets_speakers));
-        sound_onsets_speakers = sound_onsets_speakers(~isnan(sound_onsets_speakers));
-        possible_sound_onsets = sound_onsets_speakers;
-        acquisitions(file_ind).sound_trigger_time = possible_sound_onsets;
+            acquisitions(file_ind).actual_it_values = actual_it_values(possible_it_locs);
+            acquisitions(file_ind).it_times = it_times(possible_it_locs);
+            acquisitions(file_ind).directory = z(file_ind).name;
+            %save sound onsets
+            sound_onsets_speakers = [sound_condition_array(file_ind).VR_sounds{:,2}]; 
+            nan_ind = find(isnan(sound_onsets_speakers));
+            sound_onsets_speakers = sound_onsets_speakers(~isnan(sound_onsets_speakers));
+            possible_sound_onsets = sound_onsets_speakers;
+            acquisitions(file_ind).sound_trigger_time = possible_sound_onsets;
         
         end
     else
-    acquisitions(file_ind).actual_it_values = actual_it_values(possible_it_locs);
-    acquisitions(file_ind).it_times = it_times(possible_it_locs);
-    acquisitions(file_ind).directory = z(file_ind).name;
-
-    %save sound onsets
-    sound_onsets_speakers = [sound_condition_array(file_ind).VR_sounds{:,2}]; 
-    nan_ind = find(isnan(sound_onsets_speakers));
-    sound_onsets_speakers = sound_onsets_speakers(~isnan(sound_onsets_speakers));
-    possible_sound_onsets = sound_onsets_speakers;
-    acquisitions(file_ind).sound_trigger_time = possible_sound_onsets;
+        acquisitions(file_ind).actual_it_values = actual_it_values(possible_it_locs);
+        acquisitions(file_ind).it_times = it_times(possible_it_locs);
+        acquisitions(file_ind).directory = z(file_ind).name;
+    
+        %save sound onsets
+        sound_onsets_speakers = [sound_condition_array(file_ind).VR_sounds{:,2}]; 
+        nan_ind = find(isnan(sound_onsets_speakers));
+        sound_onsets_speakers = sound_onsets_speakers(~isnan(sound_onsets_speakers));
+        possible_sound_onsets = sound_onsets_speakers;
+        acquisitions(file_ind).sound_trigger_time = possible_sound_onsets;
 
     end
     figure(2);clf; hold on;plot(temp); plot(it_times(marked_its),0,'*c'); hold off
+%     figure(4);clf; hold on; plot(sync_data(:,virmen_channel_number)); plot(acquisitions(n).it_times,0,'*c');plot(acquisitions(n).it_times(find(ismember(acquisitions(n).actual_it_values,updated_trial_its.start_iti_its(file_trial_ids(n,1):file_trial_ids(n,2))))),0,'*r');plot(acquisitions(n).it_times(find(ismember(acquisitions(n).actual_it_values,updated_trial_its.end_trial_its(file_trial_ids(n,1):file_trial_ids(n,2))))),0,'*b');plot(acquisitions(n).it_times(find(ismember(acquisitions(n).actual_it_values,updated_trial_its.start_trial_its(file_trial_ids(n,1):file_trial_ids(n,2))))),0,'*g');hold off
     end
-
+    
 end
+
+figure(3);clf;hold on;title('virmen iteration values across files'); for i= 1:length(acquisitions) ; plot(acquisitions(i).it_times,acquisitions(i).actual_it_values);end;hold off
 %figure(3);clf;hold on; for i= 1:length(acquisitions) ; plot(acquisitions(i).it_times,acquisitions(i).actual_it_values);end;hold off
 %figure (3); clf;for i = 1:8; subplot(8,1,i); hold on; plot(ex_file_long(:,i)'); hold off; end
